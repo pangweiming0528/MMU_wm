d19985f178dac738dbe71e29781db9a5668355fb
pangweiming0528 <pangweiming04@gmail.com>
2025-11-17 10:37:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,24 @@
-#
+import streamlit as st
+import clips
+import logging
+
+logging.basicConfig(level=15,format='%(message)s')
+
+env = clips.Environment()
+router = clips.LoggingRouter()
+env.add_router(router)
+
+name = st.text_input("Enter your name")
+
+env.build('(deftemplate result(slot name))')
+
+env.assert_string(f'(result(name "{name}"))')
+
+env.run()
+
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result':
+        results.append(fact['name'])
+
+st.write(results[0],"output")
